3b4888067cfde8eca9f0be31f0d4278a201f998c
prashant agarwal <agarwalprashant355@gmail.com>
2020-08-10 20:42:11 +0530

    changed env urls

--- next.config.js
+++ next.config.js
@@ -16,9 +16,12 @@ module.exports = (phase, { defaultConfig }) => {
     /* config options for all phases except development here */
     env: {
       // baseURL: 'https://nextjs-theta-nine.vercel.app',
-      // fetchURL: 'http://52.66.237.36:8183',
-      fetchURL: 'https://api.mitron.tv',
-      shareURL: 'https://api.mitron.tv:443/v1/shareUrl',
+
+      shareURL: 'http://52.66.237.36:8183/v1/shareUrl',
+      fetchURL: 'http://52.66.237.36:8183',
+
+      // fetchURL: 'https://api.mitron.tv',
+      // shareURL: 'https://api.mitron.tv:443/v1/shareUrl',
     },
   };
 };
